12670da278c98de51e72cb7a3c73b30e8e90c439
Victor Notaro <victorhugonotaro@icloud.com>
2024-11-04 16:24:26 -0300

    Sua mensagem de commit aqui

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -60,7 +60,7 @@
             <a class="btn btn-outline-secondary disabled" href="#">Mais novos</a>
           </nav>
 
-        </div><!-- /.blog-main -->
+        </div><!-- /.blog-mai -->
 
         <aside class="col-md-4 blog-sidebar">
           <div class="p-3 mb-3 bg-light rounded">
